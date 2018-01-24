4b9af832924e78299daa46d79d10c4fcf6173d17
Sean Byrne <spickles@Seans-iMac.local>
2018-01-24 14:30:33 -0500

    Updated routes.rb and utuneraries controller

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -3,49 +3,49 @@ class ItinerariesController < ApplicationController
 
   # GET /itineraries
   def index
-    @itineraries = Itinerary.all
+    itineraries = Itinerary.all
 
-    render json: @itineraries
+    render json: itineraries
   end
 
   # GET /itineraries/1
   def show
-    render json: @itinerary
+    render json: itinerary
   end
 
   # POST /itineraries
   def create
-    @itinerary = Itinerary.new(itinerary_params)
+    itinerary = Itinerary.new(itinerary_params)
 
-    if @itinerary.save
-      render json: @itinerary, status: :created, location: @itinerary
+    if itinerary.save
+      render json: itinerary, status: :created
     else
-      render json: @itinerary.errors, status: :unprocessable_entity
+      render json: itinerary.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /itineraries/1
   def update
-    if @itinerary.update(itinerary_params)
-      render json: @itinerary
+    if itinerary.update(itinerary_params)
+      render json: itinerary
     else
-      render json: @itinerary.errors, status: :unprocessable_entity
+      render json: itinerary.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /itineraries/1
   def destroy
-    @itinerary.destroy
+    itinerary.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_itinerary
-      @itinerary = Itinerary.find(params[:id])
+      itinerary = Itinerary.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
     def itinerary_params
-      params.require(:itinerary).permit(:title, :month, :day, :year)
+      params.require(:itinerary).permit(:title, :intin_start, :intin_end, :creator_id)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
     collection do
       post '/login', to: 'users#login'
     end
-  end
-  resources :itineraries do
-    resources :sections
+    resources :itineraries do
+      resources :sections
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
